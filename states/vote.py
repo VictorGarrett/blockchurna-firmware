f7aabd1b2b89fe4c50ccd6413364d7151c2d1d54
diogodsg <diogodasilvagouveia@gmail.com>
2024-12-11 18:46:09 -0300

    first_render

--- states/vote.py
+++ states/vote.py
@@ -149,6 +149,7 @@ class VoteState(State):
 
     def render(self, screen):
         if self.first_render:
+            self.timeout = TIMEOUT
             self.first_render=False
             self.timer = Timer(1.0, self.reset_state)
             self.timer.start()
